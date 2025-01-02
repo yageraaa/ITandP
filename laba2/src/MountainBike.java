324a1d4c67654f916883efa031b356a6b176e672
German <149824373+yageraaa@users.noreply.github.com>
2025-01-02 19:36:20 +0300

    Add files via upload

---
+++ laba2/src/MountainBike.java
@@ -0,0 +1,35 @@
+public class MountainBike extends Bicycle {
+    private String suspensionType;
+    private static int mountainBikeCount = 0;
+
+    public MountainBike(String brand, int numberOfGears, double weight, String suspensionType) {
+        super(brand, numberOfGears, weight);
+        this.suspensionType = suspensionType;
+        mountainBikeCount++;
+    }
+
+    public MountainBike() {
+        this("Default", 21, 12.0, "Одноподвесная");
+    }
+
+    public String getSuspensionType() {
+        return suspensionType;
+    }
+
+    public void setSuspensionType(String suspensionType) {
+        this.suspensionType = suspensionType;
+    }
+
+    @Override
+    public void ride() {
+        System.out.println("Горный велосипед едет в гору");
+    }
+
+    public void jump() {
+        System.out.println("Горный велосипед прыгает");
+    }
+
+    public static int getMountainBikeCount() {
+        return mountainBikeCount;
+    }
+}
