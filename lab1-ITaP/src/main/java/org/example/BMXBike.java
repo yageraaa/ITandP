85f93fb6012505b2172fef3af62b787304558ae3
German Berezin <149824373+yageraaa@users.noreply.github.com>
2025-06-01 18:08:32 +0300

    Add lab1 files

---
+++ lab1-ITaP/src/main/java/org/example/BMXBike.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class BMXBike extends Bicycle {
+    private boolean hasPegs;
+
+    public BMXBike(String brand, int numberOfGears, double weight, boolean hasPegs) {
+        super(brand, numberOfGears, weight);
+        this.hasPegs = hasPegs;
+    }
+
+    public BMXBike() {
+        this("Unknown", 1, 9.0, true);
+    }
+
+    public boolean isHasPegs() {
+        return hasPegs;
+    }
+
+    public void setHasPegs(boolean hasPegs) {
+        this.hasPegs = hasPegs;
+    }
+
+    @Override
+    public void ride() {
+        System.out.println("BMX выполняет трюк.");
+    }
+
+    public void barspin() {
+        System.out.println("BMX вращает руль.");
+    }
+}
